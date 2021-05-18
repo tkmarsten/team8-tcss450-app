df3ff718e50c8f4a5cd36f79371d178233e873c7
tkmarsten <tkmarsten@gmail.com>
2021-05-17 20:08:51 -0700

    Testing

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,8 +6,16 @@ const pool = require('../utilities/exports').pool
 
 const router = express.Router()
 
-router.get("/:email?", (request, response) => {
-    let query = `SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.Email FROM Members WHERE MemberID IN (SELECT MemberID_A FROM Contacts WHERE MemberID_A = $1)`
+router.get("/:email", (request, response, next) => {
+    if (!request.params.email) {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    } else {
+        next();
+    }
+}, (request, response) => {
+    let query = `SELECT Members.FirstName, Members.LastName, Members.Nickname, Members.Email FROM Members WHERE MemberID IN (SELECT MemberID_A FROM Contacts WHERE MemberID_A = 8)`
     let values = [request.decoded.memberid]
 
     pool.query(query, values)
