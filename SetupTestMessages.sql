1999b872eda76dee4c4dd50fb1a14814cf6047e7
NicoRoberts <33466452+NicoRoberts@users.noreply.github.com>
2021-05-18 21:57:27 -0700

    added test.edu to the global chat

--- SetupTestMessages.sql
+++ SetupTestMessages.sql
@@ -52,6 +52,7 @@ FROM Members
 WHERE Members.Email='test1@test.com'
     OR Members.Email='test2@test.com'
     OR Members.Email='test3@test.com'
+    OR Members.Email='test@test.edu'
 RETURNING *;
 
 --Add Multiple messages to create a conversation
