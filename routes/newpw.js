63747b50905fd1d9b4c3ecd003a7d8e965533d1b
Marc Perez <marc.perez.dev@gmail.com>
2021-05-20 04:46:25 -0700

    add POST option for newpw route

--- routes/newpw.js
+++ routes/newpw.js
@@ -13,6 +13,8 @@ const generateSalt = require('../utilities').generateSalt
 const sendEmail = require('../utilities').sendEmail
 
 router.get("/", (request, response) => {
+    const passwordIn = request.body.password
+
     if (isStringProvided(request.query.e1)
         && isStringProvided(request.query.e2)
         && isStringProvided(request.query.e3)) {
@@ -21,8 +23,57 @@ router.get("/", (request, response) => {
         
         // Send new password to user.
         let password = generatePassword(8) // Default is a randomly generated password.
-        if (isStringProvided(request.body.password)){
-            password = request.body.password
+        if (isStringProvided(passwordIn)){
+            password = passwordIn
+        }
+
+        let salt = generateSalt(32)
+        let salted_hash = generateHash(password, salt)
+        
+        let theQuery = "UPDATE Members SET Password = $1, Salt = $2 WHERE Email = $3"
+        let values = [salted_hash, salt, email]
+
+        pool.query(theQuery, values)
+            .then(result => {
+                // Inform user of new password.
+                sendEmail(process.env.SENDER_EMAIL, email,
+                    "New Password", "Your new password is: " + password)
+                
+                //Success message
+                response.status(201).send({
+                    success: true,
+                    message: "success, please check your email for your new password."
+                })
+            })
+            .catch((error) => {
+                //log the error
+                console.log(error)
+                // if (error.constraint == "members_username_key") {
+                //     response.status(400).send({
+                //         message: "Username exists"
+                //     })
+                // }
+            })
+    } else {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    }
+})
+
+router.post("/", (request, response) => {
+    const passwordIn = request.body.password
+
+    if (isStringProvided(request.query.e1)
+        && isStringProvided(request.query.e2)
+        && isStringProvided(request.query.e3)) {
+        // Reconstitute email address from link parameters TODO decode
+        let email = request.query.e1 + "@" + request.query.e2 + "." + request.query.e3
+        
+        // Send new password to user.
+        let password = generatePassword(8) // Default is a randomly generated password.
+        if (isStringProvided(passwordIn)){
+            password = passwordIn
         }
 
         let salt = generateSalt(32)
